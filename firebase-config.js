8a41198945f7078b0fffc36ea9a8cb9083e319db
vigourjp2 <vigourjp2@gmail.com>
2026-06-21 08:37:45 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Realtime Database 設定ファイル
+// 参考ファイル(index.html)内の firebaseConfig を学習テスト用に転記済み。
+// 全ページは window.GAKUSHU_FIREBASE_CONFIG を読む。
+window.GAKUSHU_FIREBASE_CONFIG = {
+  "apiKey": "AIzaSyCuC8k-Bkrj3MFcExjsWjnVxQR0WMYeTlY",
+  "authDomain": "minicra.firebaseapp.com",
+  "databaseURL": "https://minicra-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "minicra",
+  "storageBucket": "minicra.firebasestorage.app",
+  "messagingSenderId": "943025034725",
+  "appId": "1:943025034725:web:c46da7ded946ce4fde0b0c"
+};
